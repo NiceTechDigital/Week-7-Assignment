bab32320d84d0c749d1d95c25aba16f849f4e2ac
NiceTechDigital <henryasha007@gmail.com>
2025-05-08 18:37:04 +0100

    Update Item.js

--- Item.js
+++ Item.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const itemScheme = new mongoose.Schema({
-    itemName: {type: String, require: true},
-    description: {type: String, require: true},
-    locationFound: {type: String, require: true},
-    dateFound: {type: Date, require: true},
-    claimed: {type: Boolean, default: false},
+const itemSchema = new mongoose.Schema({
+  itemName: { type: String, required: true },
+  description: { type: String, required: true },
+  locationFound: { type: String, required: true },
+  dateFound: { type: Date, required: true },
+  claimed: { type: Boolean, default: false }
+});
 
-}, {timestamps: true});
-
-module.exports = mongoose.model('Item', itemScheme);
+module.exports = mongoose.model('Item', itemSchema); 
