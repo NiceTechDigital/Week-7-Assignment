70830680c8bed82b0f28059d38968611ea086fdf
NiceTechDigital <henryasha007@gmail.com>
2025-05-08 18:44:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const itemRoutes = require('./itemRoutes');
 
-const itemRoutes = require('./routes/itemRoutes');
+dotenv.config();
+
 const app = express();
-
-app.use(cors());
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(()=> console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/items', itemRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Lost & Found API is running');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () =>
+      console.log('Server running & DB connected')
+    );
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
